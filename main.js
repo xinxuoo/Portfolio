7b15a5de8331d4394284df7209ef5589e24c5cb9
xinxuoo <jmj3634@naver.com>
2022-08-03 14:46:53 +0900

    JS : home contact button

--- main.js
+++ main.js
@@ -19,7 +19,16 @@ navbarMenu.addEventListener('click', (event) => {
   if(link == null){
     return;
   }
-  console.log(event.target.dataset.link);
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({behavior: 'smooth'});
+  scrollIntoView(link);
 });
+
+// Handle click on 'contact me' button on home
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
+});
+
+function scrollIntoView(selctor){
+  const scrollTo = document.querySelector(selctor);
+  scrollTo.scrollIntoView({behavior: 'smooth'});
+}
