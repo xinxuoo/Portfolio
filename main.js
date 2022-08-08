3572c914e696010b3595f2e75b0f96a2b27a294a
xinxuoo <jmj3634@naver.com>
2022-08-08 16:05:08 +0900

    JS : Navbar menu toggle

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ document.addEventListener('scroll' , () => {
     navbar.classList.remove('navbar--dark');
   }
 });
-
 // Handel scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
@@ -19,7 +18,14 @@ navbarMenu.addEventListener('click', (event) => {
   if(link == null){
     return;
   }
+  navbarMenu.classList.remove('open');
   scrollIntoView(link);
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click' , ()=> {
+  navbarMenu.classList.toggle('open');
 });
 
 // Handle click on 'contact me' button on home
